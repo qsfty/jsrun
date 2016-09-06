16a53f8a2ee7690db27e2c827d7b69daa963ba99
minqiang.li <minqiang.li@fraudmetrix.cn>
2016-09-06 23:48:36 +0800

    ok

--- database/config/jdbc.js
+++ database/config/jdbc.js
@@ -6,14 +6,6 @@ module.exports = {
             password : "123456",
             database : "jsrun",
             dateStrings : "DATETIME"
-        },
-        portal : {
-            host : "sqld.duapp.com",
-            port : 4050,
-            user : "a2b0b6b656114636bd6fb53668b8767b",
-            password : "3a4b116fe8ef4d018db57d43110c763d",
-            database : "gmcyOmlNgVatvdSaXiEv",
-            dateStrings : "DATETIME"
         }
     },
     debug : true,
